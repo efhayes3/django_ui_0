e10e8c025da2df9201aa11fb6a0e253c1da72091
Edward Hayes <efhayes3@cs.uchicago.edu>
2017-02-25 18:28:35 -0600

    commit all

---
+++ web/search/data_functions.py
@@ -0,0 +1,8 @@
+
+
+def fetch_current_data(neighborhood_code):
+	return "These are the results."
+
+def calculate_with_alt_param(neighborhood_code, altered_parameters):
+	new_med_sale = 150000
+	return 'New Median Price: $%d'.format(new_med_sale)
--- web/search/views.py
+++ web/search/views.py
@@ -1,11 +1,18 @@
+# 
+# ATTN Graders: Some of the structure in this file was inspiried 
+# by the django UI used in PA3
+# 
+
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
 from .forms import SubmitNeighborhood, SubmitAlteredParameters
+from .data_functions import fetch_current_data, calculate_with_alt_param
 
 
 def start(request):
-    res = None
+    current_neighborhood_data = None
+    result2 = None
     context = {}
 
     if request == 'GET':
@@ -13,13 +20,20 @@ def start(request):
         select_neighborhood_form = SubmitNeighborhood(request.GET)
 
         neighborhood_code = select_neighborhood_form.cleaned_data['neighborhood_code']
-
         current_neighborhood_data = fetch_current_data(neighborhood_code)
     else:
         select_neighborhood_form = SubmitNeighborhood()
     
-    context['select_neighborhood_form'] = select_neighborhood_form
 
+    if current_neighborhood_data == None:
+        context['current_neighborhood_data'] = None
+        alter_form = None
+    else:
+        context['current_neighborhood_data'] = current_neighborhood_data
+        alter_form = SubmitAlteredParameters()
+
+    context['select_neighborhood_form'] = select_neighborhood_form
+    context['alter_form'] = alter_form
     return render(request, 'index.html', context)
 
 
